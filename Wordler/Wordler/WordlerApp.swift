fe1108427969b48e8ded09a9a3c2273ce462224c
dennymathewfp <denny.mathew@fairprice.com.sg>
2022-02-12 15:17:48 +0800

    [WD-0] Initializes

---
+++ Wordler/Wordler/WordlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordlerApp.swift
+//  Wordler
+//
+//  Created by MATHEW DENNY on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WordlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
